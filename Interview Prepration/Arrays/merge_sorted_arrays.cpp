b37b9e1b94e111a53e71d42ad61e0195335ba57f
it5prasoon <prasoonk187@gmail.com>
2021-05-26 15:45:16 +0530

    :rocket: Merge two sorted arrays without using extra space, Binary search algorithm

--- Interview Prepration/Arrays/merge_sorted_arrays.cpp
+++ Interview Prepration/Arrays/merge_sorted_arrays.cpp
@@ -1,29 +1,36 @@
-// Merge 2 sorted arrays without using Extra space.
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void mergeArrays(int a[], int b[], int n, int m)
+void merge(int arr1[], int arr2[], int n, int m)
 {
-	map<int, bool> mp;
-	
-    for(int i = 0; i < n; i++)
-	mp[a[i]] = true;
-	
-	for(int i = 0;i < m;i++)
-	mp[b[i]] = true;
-	
-	for(auto i: mp)
-	cout<< i.first <<" ";
+	int i = 0, j = 0, k = n - 1;
+	while (i <= k and j < m) {
+		if (arr1[i] < arr2[j])
+			i++;
+		else {
+			swap(arr2[j++], arr1[k--]);
+		}
+	}
+
+	sort(arr1, arr1 + n);
+	sort(arr2, arr2 + m);
 }
 
+// Driver Code
 int main()
 {
-	int a[] = {1, 3, 5, 7}, b[] = {2, 4, 6, 8};
-	
-	int size = sizeof(a)/sizeof(int);
-	int size1 = sizeof(b)/sizeof(int);
-	mergeArrays(a, b, size, size1);
-	
+
+	int ar1[] = { 1, 5, 9, 10, 15, 20 };
+	int ar2[] = { 2, 3, 8, 13 };
+	int m = sizeof(ar1) / sizeof(ar1[0]);
+	int n = sizeof(ar2) / sizeof(ar2[0]);
+	merge(ar1, ar2, m, n);
+
+	cout << "After Merging \nFirst Array: ";
+	for (int i = 0; i < m; i++)
+		cout << ar1[i] << " ";
+	cout << "\nSecond Array: ";
+	for (int i = 0; i < n; i++)
+		cout << ar2[i] << " ";
 	return 0;
 }
