bba4b2df43f390c0c2fd70e0dba61a4f57ec6aed
Prasoon Kumar <prasoonk187@gmail.com>
2021-08-04 23:27:47 +0530

    binary tree added

--- 5. Tree-based data structures/Binary Tree/implementation_btree.cpp
+++ 5. Tree-based data structures/Binary Tree/implementation_btree.cpp
@@ -4,18 +4,21 @@
 using namespace std;
 
 class BTree {
-    int data;
     BTree *left, *right;
 
     public:
+        int data;
+
         BTree();
         BTree(int);
         BTree* createBTree(BTree* root);
         BTree* Insert(BTree*, int);
         void Inorder(BTree*);
-        bool Search(BTree*, int);
-        int FindMin(BTree*);
-        int FindMax(BTree*);
+        int findMax(BTree *root);
+        int findMin(BTree *root);
+        int size(BTree *root);
+        int height(BTree *root);
+        BTree* deepestNode(BTree *root);
 };
 
 // Default Constructor definition.
@@ -61,6 +64,100 @@ BTree* BTree ::Insert(BTree* root, int value) {
     return root;
 }
 
+int BTree ::findMax(BTree* root) {
+    int temp, left, right, max = INT32_MIN;
+    if (!root) {
+        return -1;
+    }
+
+    temp = root->data;
+    left = findMax(root->left);
+    right = findMax(root->right);
+
+    if(left > right) {
+        max = left;
+    } else max = right;
+
+    if(temp > max) {
+        max = temp;
+    }
+
+    return max;
+}
+
+int BTree ::findMin(BTree* root) {
+    int temp, left, right, min = INT32_MAX;
+    if (!root) {
+      return min;
+    }
+
+    temp = root->data;
+    left = findMin(root->left);
+    right = findMin(root->right);
+
+    if(left < right) {
+        min = left;
+    } else min = right;
+
+    if(temp < min) {
+        min = temp;
+    }
+
+    return min;
+}
+
+int BTree ::size(BTree *root) {
+    if (!root) {
+        return 0;
+    } else {
+        return(size(root->left) + 1 + size(root->right));
+    }
+}
+
+int BTree ::height(BTree *root) {
+    int leftheight, rightheight;
+
+    if (!root) {
+        return 0;
+    } else {
+        leftheight = height(root->left);
+        rightheight = height(root->right);
+
+        if (leftheight > rightheight) 
+            return leftheight+1;
+        else
+            return rightheight+1;
+    }
+}
+
+BTree* BTree ::deepestNode(BTree *root) {
+    BTree* temp = new BTree();
+    queue<BTree*> q;
+
+    if(!root) {
+        return NULL;
+    }
+
+    q.push(root);
+
+    while (!q.empty()) {
+        temp = q.front();
+        q.pop();
+
+        if(temp->left)
+            q.push(temp->left);
+        if(temp->right)
+            q.push(temp->right);
+    }
+
+    while(!q.empty()) {
+        q.pop();
+    }
+
+    return temp;
+}
+
+
 void BTree ::Inorder(BTree* root) {
     if (!root) {
         return;
@@ -83,15 +180,23 @@ BTree* BTree:: createBTree(BTree* root) {
 
 int main()
 {
-    BTree b, *root;
+    BTree b, *root, *deepnode;
 
     root = b.createBTree(root);
     b.Inorder(root);
+    cout << "Size is: " << b.size(root) << endl;
 
     cout << "==========================================\n";
     root = b.Insert(root, 12);
- 
     b.Inorder(root);
 
+    cout << "Size is: " << b.size(root) << endl;
+    cout << "Maximum number: " << b.findMax(root) << "\n";
+    cout << "Minimum number: " << b.findMin(root) << "\n";
+    cout << "Height: " << b.height(root) << "\n";
+
+    deepnode = b.deepestNode(root);
+    cout << "Deepest Node: " << deepnode->data;
+    
     return 0;
 }
